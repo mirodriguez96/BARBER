2b7cccafe25cae8ba3b941b00a8db5715c1d1213
Miguel Rodriguez <mirodriguezor@unal.edu.co>
2026-06-01 01:20:15 -0500

    Fix tests de autenticacion

--- barberia/backends.py
+++ barberia/backends.py
@@ -3,6 +3,13 @@ from django.contrib.auth.backends import ModelBackend
 
 
 class SafeAxesBackend(AxesStandaloneBackend):
+    def user_can_authenticate(self, user):
+        is_active = getattr(user, "is_active", None)
+        return is_active or is_active is None
+
+    def get_user(self, user_id):
+        return ModelBackend.get_user(self, user_id)
+
     def authenticate(self, request, username=None, password=None, **kwargs):
         if request is None:
             return ModelBackend.authenticate(
